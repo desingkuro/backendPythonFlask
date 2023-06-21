1626406f7a49055a69d034d1a80528416717f785
desingkuro <andresmauricio745gmail.com>
2023-06-21 08:25:35 -0500

    se configuro para descargar segun el itag enviado

--- main.py
+++ main.py
@@ -18,83 +18,29 @@ audio_resolutions = {}
 def inicio():
     return 'Bienvenido'
 
-def recibir():
-    auth_header = request.headers.get('Authorization')
-    if auth_header and auth_header == 'Bearer anmalima601262':
-        if request.method == 'POST':
-            link = request.form['texto']
-            yt = YouTube(link)
-            video_resolutions = {}
-            audio_resolutions = {}
-            streams = yt.streams
-            print('entro')
-            for stream in streams:
-                if 'mp4' in stream.mime_type:
-                    if stream.type == 'video':
-                        if stream.resolution not in video_resolutions:
-                            video_resolutions[stream.resolution] = stream.mime_type
-                    elif stream.type == 'audio':
-                        if stream.abr not in audio_resolutions:
-                            audio_resolutions[stream.abr] = stream.mime_type
-            result = {
-                'video': video_resolutions,
-                'audio': audio_resolutions
-            }
-            return jsonify(result)
-        else:
-            return jsonify({'message': 'false'})
-    else:
-        return jsonify({'message': 'no autorizado'})
-
-@app.route('/link', methods=['POST', 'GET'])
-def link():
-    auth_header = request.headers.get('Authorization')
-    if auth_header and auth_header == 'Bearer anmalima601262':
-        if request.method == 'POST':
-            texto = request.form['texto']
-            print('Recibido: ' + texto)
-            yt = YouTube(texto)
-            audio = yt.streams.filter(only_audio=True).first()
-            download_path = os.path.join(os.path.expanduser('~'), 'Downloads')
-            file_path = os.path.join(download_path, audio.default_filename)
-            print('Descargando')
-            # Descargar el archivo de audio
-            audio.download(download_path)
-            print('Descargado')
-
-            # descarga el 
-            response = send_file(file_path, as_attachment=True)
-
-            return response
-        else:
-            return 'No se envió nada'
-    else:
-        return 'Acceso no autorizado', 401
-
-@app.route('/descargar', methods=['POST', 'GET'])
+@app.route('/descargar', methods=['POST'])
 def descargar():
     auth_header = request.headers.get('Authorization')
     if auth_header and auth_header == 'Bearer anmalima601262':
         data = request.json
         link = data.get('texto')
         itag = data.get('itag')
-        yt = YouTube(link)
-        stream = yt.streams.get_by_itag(itag)
         print(f"Link recibido: {link}")
         print(f"ITAG recibido: {itag}")
-        if stream is not None:
-            print("Entró en el if")
-            # Descargar el archivo en el directorio actual
-            file_path = f'descarga.{stream.subtype}'
-            stream.download(filename=file_path)
+        yt = YouTube(link)
+        video = yt.streams.get_by_itag(itag)
 
-            # Establecer el encabezado Content-Disposition y enviar el archivo
-            filename = os.path.basename(file_path)
-            response = make_response(send_file(file_path))
-            response.headers.set('Content-Disposition', 'attachment', filename=filename)
+        if video.includes_audio_track:
+            print("El video contiene audio")
+            download_path = os.path.join(os.path.expanduser('~'), 'Downloads')
+            file_path = os.path.join(download_path, video.default_filename)
+            # Descargar el archivo de video
+            video.download(download_path)
+            print('Descargado')
+            response = send_file(file_path, as_attachment=True)
             return response
         else:
-            return jsonify({'message': 'No se encontró ninguna transmisión con el itag especificado'})
+            return jsonify({'message': 'El video no contiene audio'})
     else:
         return jsonify({'message': 'No autorizado'})
 
